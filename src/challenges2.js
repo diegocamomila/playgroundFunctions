e922141b3383f5ed8f876ae84b00e7e282087d3c
diegocamomila <diego.camomila@gmail.com>
2022-12-08 20:07:02 -0300

    FEAT: Adicionará a funçao generatePhoneNumber em src/challenges.js

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,55 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  if (tech.length <= 0) {
+    return 'Vazio!';
+  }
+
+  let newArray = [];
+  let techSort = tech.sort();
+
+  for (let index = 0; index < techSort.length; index += 1) {
+    let objList = {
+      tech: techSort[index],
+      name,
+    };
+
+    newArray.push(objList);
+  }
+  return newArray;
 }
 
+// complemento desafio 11
+const test = (listNumbers) => {
+  listNumbers.sort();
+  let current = null;
+  let cnt = 0;
+
+  for (let index = 0; index < listNumbers.length; index += 1) {
+    if (listNumbers[index] !== current) {
+      current = listNumbers[index];
+    } else cnt += 1;
+  }
+
+  if (cnt === 3) {
+    return true;
+  }
+};
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(listNumbers) {
+  const max = (Math.max.apply(null, listNumbers));
+  const min = (Math.min.apply(null, listNumbers));
+
+  if (listNumbers.length !== 11) return 'Array com tamanho incorreto.';
+  if (max > 9 || min < 0) return 'não é possível gerar um número de telefone com esses valores';
+  if (test(listNumbers) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let phone = `(${listNumbers.slice(0, 2).join('')}) ${listNumbers.slice(2, 7).join('')}-${
+    listNumbers.slice(7, 11).join('')}`;
+  return phone;
 }
 
 // Desafio 12
